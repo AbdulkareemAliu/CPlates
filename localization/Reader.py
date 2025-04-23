acaea37924ff805686ddb321b9466b1b8d6e0e14
Aman <emanant@mit.edu>
2025-04-23 17:01:42 -0400

    first version live Reader self localization using anchors

--- localization/Reader.py
+++ localization/Reader.py
@@ -2,11 +2,13 @@ import mercury
 from datetime import datetime, timezone
 from time import sleep
 from dataclasses import dataclass, asdict
+
+import sys 
+print(sys.executable)
+
 import pandas as pd
 import numpy as np
 from scipy.optimize import minimize 
-
-
 @dataclass
 class ReadingData:
     EPC: list
@@ -32,6 +34,11 @@ class ThingMagic(Reader):
         self.reader.set_region("NA")
         self.reader.set_read_plan([1], "GEN2", read_power=3000)
         self.save_dir = '/home/eant/splitRF/data/reader_data'
+        self.anchor_tags_rssi = {
+            "D4C3204700000000000000C4": None,
+            "E135204700000000000000CA": None,
+            "E23456780000000000000031": None
+        }
 
     def sync_reading(self):
         tag_list = self.reader.read()
@@ -56,6 +63,12 @@ class ThingMagic(Reader):
                     ix = tag_data.EPC.index(tag.epc.decode("utf-8"))
                     for tag_val in tag_data_list:
                         tag_val.pop(ix)
+            
+            #update anchor tag rssi values
+            if tag.epc.decode("utf-8") in self.anchor_tags_rssi:
+                # print(tag.epc.decode("utf-8"))
+                self.anchor_tags_rssi[tag.epc.decode("utf-8")] = tag.rssi
+            
             for tag_val, val in zip(tag_data_list, [tag.epc.decode("utf-8"), tag.rssi, tag.phase,
                                                     datetime.fromtimestamp(tag.timestamp).astimezone(
                                                             timezone.utc)]):
@@ -65,6 +78,7 @@ class ThingMagic(Reader):
         self.reader.start_reading(lambda tag: read_update(tag, tag_data, replace))
         try:
             while True:
+                self.localize_reader()
                 sleep(1)
         except KeyboardInterrupt:
             self.reader.stop_reading()
@@ -73,30 +87,29 @@ class ThingMagic(Reader):
         return tag_data
 
     def distance(self, rssi):
-        d = 10**((-70-(rssi))/(10*2.5))
+        d = 10**((-72-(rssi))/(10*2.5))
         return d
     
     
     def localize_reader(self):
 
-        # anchor1 = "abc"
-        # anchor2 = "def"
-        # anchor3 = "ghi"
-
-        anchor1_rssi = -30
-        anchor2_rssi = -20
-        anchor3_rssi = -110
-
+        # fixed positions 
         anchor_positions = np.array([
             [0.0, 0.0, 0.0], # Anchor 1
             [2.0, 0.0, 0.0], # Anchor 2
             [1.0, 2.0, 1.0]  # Anchor 3
         ])
 
+        for anchor in self.anchor_tags_rssi:
+            if self.anchor_tags_rssi[anchor] == None:
+                print(self.anchor_tags_rssi)
+                print("Cannot Localize Reader: Anchors Not Found")
+                return
+
         distances = np.array([
-            self.distance(anchor1_rssi),
-            self.distance(anchor2_rssi),
-            self.distance(anchor3_rssi)
+            self.distance(self.anchor_tags_rssi["D4C3204700000000000000C4"]),
+            self.distance(self.anchor_tags_rssi["E135204700000000000000CA"]),
+            self.distance(self.anchor_tags_rssi["E23456780000000000000031"])
         ])
 
         def objective(reader_pos):
@@ -114,10 +127,10 @@ class ThingMagic(Reader):
 
 if __name__ == "__main__":
     reader = ThingMagic()
-    reader.localize_reader()
-    # tag_data = reader.async_reading(False)
-    # tag_df = pd.DataFrame(asdict(tag_data))
-    # now_strtime = datetime.now().strftime('%Y%m%d%H%M%S')
-    # print(tag_df)
-    # tag_df.to_csv(f'{reader.save_dir}/rfdata_{now_strtime}.csv')
+    # reader.localize_reader()
+    tag_data = reader.async_reading(False)
+    tag_df = pd.DataFrame(asdict(tag_data))
+    now_strtime = datetime.now().strftime('%Y%m%d%H%M%S')
+    print(tag_df)
+    tag_df.to_csv(f'{reader.save_dir}/rfdata_{now_strtime}.csv')
     # print(reader.sync_reading())
